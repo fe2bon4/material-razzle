24bde7d0b0ae43fc6a3514791666027801e13899
Bon Lazaga <blazaga@dnamicro.com>
2021-04-16 13:16:42 +0800

    add middleware

--- src/server/middlewares.ts
+++ src/server/middlewares.ts
@@ -14,19 +14,42 @@ const log_middleware = (
   next();
 };
 
+const auth_middleware = (req: Request, res: Response, next: NextFunction) => {
+
+  const { 
+    BASIC_AUTH_ENABLE = undefined,
+    BASIC_AUTH_USERNAME = '',
+    BASIC_AUTH_PASSWORD = ''
+  } = process.env
+
+  if(!BASIC_AUTH_ENABLE) {
+    return next();
+  }
+  
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  if (login && password && login === BASIC_AUTH_USERNAME && password === BASIC_AUTH_PASSWORD) {
+    // Access granted...
+    return next()
+  }
+
+  res.set('WWW-Authenticate', 'Basic realm="401"')
+  return  res.status(401).send({
+    code: '401',
+    message: 'Authentication required.'
+  })
+
+  // -----------------------------------------------------------------------
+
+}
+
 
 // Define Middleware Here
 export default ( server: Express )  => {
+
   server.use('*', log_middleware);
 
-
-  server.use(
-    '/socket.io',
-    createProxyMiddleware({
-      target: 'http://localhost:3030',
-      ws: true,
-    })
-  );
-
+  server.use('*', auth_middleware)
 
 }
